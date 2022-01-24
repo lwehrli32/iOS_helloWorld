34b6c20dc1ba6db1c7383821822f7003fcb8d02a
Lukas <lukaswehrli32@gmail.com>
2022-01-23 21:19:15 -0600

    Initial Commit

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Lukas Wehrli on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
