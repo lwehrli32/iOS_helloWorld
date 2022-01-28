cdd101cb1082b92694bc2b93d202f036ea5f8fcb
Lukas <lukaswehrli32@gmail.com>
2022-01-28 12:23:49 -0600

    Finished learning about combinging views

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,17 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Description of Turtle Rock")
                     .font(.subheadline)
-                Text("California")
-                    .font(.subheadline)
+                    .foregroundColor(.secondary)
             }
+            .padding()
+            
+            Spacer()
         }
-        .padding()
     }
 }
 
